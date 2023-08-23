153f1b919ca0c6e493769be2c5586985345ed2ae
korsarking <korsarkings@gmail.com>
2023-08-23 15:13:45 +0200

    Implemented "Get list" of users endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from rest_framework.routers import DefaultRouter
+
 from apps.users.views import RegisterUserView
+from apps.users.views import ListUserViewSet
+
+router = DefaultRouter()
+router.register(r'users', ListUserViewSet)
 
 urlpatterns = [
     path("register", RegisterUserView.as_view(), name="token_register"),
     path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
+urlpatterns += router.urls
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,21 +1,25 @@
 from django.contrib.auth import get_user_model
 from drf_util.decorators import serialize_decorator
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from apps.users.serializers import UserSerializer
+from apps.users.serializers import RegisterUsersSerializer, ListUsersSerializer
 
 User = get_user_model()
 
 
 class RegisterUserView(GenericAPIView):
-    serializer_class = UserSerializer
+    serializer_class = RegisterUsersSerializer
+    queryset = User.objects.all()
 
     permission_classes = (AllowAny,)
     authentication_classes = ()
 
-    @serialize_decorator(UserSerializer)
+    @serialize_decorator(serializer_class)
     def post(self, request):
         validated_data = request.serializer.validated_data
 
@@ -33,4 +37,15 @@ class RegisterUserView(GenericAPIView):
         user.set_password(password)
         user.save()
 
-        return Response(UserSerializer(user).data)
+        refresh_token = RefreshToken().for_user(request.user)
+
+        return Response({
+            "refresh": str(refresh_token),
+            "access": str(refresh_token.access_token)
+        })
+
+
+class ListUserViewSet(ListModelMixin, GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = ListUsersSerializer
+    permission_classes = (IsAuthenticated,)
