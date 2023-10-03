644c269a071f1f90fc7c95df0738ab97e1b6b8b6
korsarking <korsarkings@gmail.com>
2023-10-03 08:48:28 +0200

    remove comment lines

--- config/settings.py
+++ config/settings.py
@@ -240,26 +240,5 @@ LOGGING = {
     },
 }
 
-# LOGGING = {
-#     "version": 1,
-#     "filters": {
-#         "require_debug_true": {
-#             "()": "django.utils.log.RequireDebugTrue",
-#         }
-#     },
-#     "handlers": {
-#         "console": {
-#             "level": "DEBUG",
-#             "filters": ["require_debug_true"],
-#             "class": "logging.StreamHandler",
-#         }
-#     },
-#     "loggers": {
-#         "django.db.backends": {
-#             "level": "DEBUG",
-#             "handlers": ["console"],
-#         }
-#     },
-# }
 
 env.seal()
